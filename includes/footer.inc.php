84cf5792e17ab99966df22389e0d556e48da9d0b
18029801724 <819993529@qq.com>
2017-08-30 22:03:54 +0800

    Add files via upload

---
+++ includes/footer.inc.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* TestGuest Version1.0
+* ================================================
+* Copy 2010-2012 yc60
+* Web: http://www.yc60.com
+* ================================================
+* Author: Lee
+* Date: 2010-8-10
+*/
+//防止恶意调用
+if (!defined('IN_TG')) {
+	exit('Access Defined!');
+}
+_close();
+?>
+<div id="footer">
+	<p>本页面加载耗时为: <?php echo  round((_runtime() - START_TIME),4)?>秒</p>
+	<p>版权所有 翻版必究</p>
+</div>
